8e2f0660cf0c4e1870f49817e96e1699b8f17a90
= <javidbu@gmail.com>
2017-11-14 23:27:36 +0100

    Now you can specify a default author and category for your blog posts
    Also, it's now PEP-8 friendly ;)

--- README.md
+++ README.md
@@ -6,3 +6,4 @@ Small plugin for creating drafts and publishing them with Sublime Text
 1. Unzip or clone this repo in that Folder
 1. To use it, simply press `command + shift + P` and search for `jekyll`. You can select `New draft` or `Publish draft`
 1. Drafts must be saved in the `_drafts` directory, and posts in the `_posts` directory
+1. You can edit the `jekyll.sublime-settings` file with your name and the default category for your blog
--- sublime_jekyll.py
+++ sublime_jekyll.py
@@ -3,22 +3,43 @@ import sublime_plugin
 import os
 from datetime import datetime
 
+SETTINGS = 'jekyll.sublime-settings'
+
+
 class NewDraftCommand(sublime_plugin.WindowCommand):
     ''' Asks the user for the title of the new draft, and creates it '''
     def run(self):
-        self.window.show_input_panel('Título: ', '', self.input_done, None, None)
+        self.window.show_input_panel(
+            'Título: ', '', self.input_done, None, None)
 
     def input_done(self, text):
         self.window.new_file()
         active_editor = self.window.active_view()
+        author = sublime.load_settings(SETTINGS).get("author")
         if active_editor:
-            active_editor.run_command('insert',
-                {'characters': '---\nlayout: post\ntitle: "{0}"\ncategories: \ncomments: true\n---\n\n'.format(text)})
+            active_editor.run_command(
+                'insert', {'characters': '''---
+layout: post
+title: "{0}"
+author: "{1}"
+categories: {2}
+comments: true
+#images:
+
+#  - url: /assets/img/
+#    alt:
+#    title:
+---
+
+'''.format(text, author)})
+            active_editor.run_command()
+
 
 class PublishDraftCommand(sublime_plugin.TextCommand):
-    ''' Publish current draft, adding the date in UTC timezone '''
+    ''' Publish current draft '''
     def run(self, edit):
-        date = '\ndate: {0}'.format(datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S'))
+        date = '\ndate: {0}'.format(datetime.utcnow().strftime(
+            '%Y-%m-%d %H:%M:%S'))
         self.view.insert(edit, self.view.text_point(5, 0) - 1, date)
         self.view.run_command('save')
         if len(self.view.file_name()) > 0:
